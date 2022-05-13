064d1e3dc78356fdf8f3fb1044f9328c2fa7795c
Pedro Almeida <ip4dro@gmail.com>
2022-05-13 20:03:47 +0200

    Make Weak conform to Hashable

--- Sources/Coordinator/WeakObject.swift
+++ Sources/Coordinator/WeakObject.swift
@@ -1,9 +1,21 @@
 import UIKit
 
 public final class Weak<Object: AnyObject> {
+    private let objectIdentifier: ObjectIdentifier
     public weak var weakReference: Object?
 
     public init(_ object: Object) {
+        self.objectIdentifier = ObjectIdentifier(object)
         self.weakReference = object
+    }
+}
+
+extension Weak: Hashable {
+    public static func == (lhs: Weak<Object>, rhs: Weak<Object>) -> Bool {
+        lhs.objectIdentifier == rhs.objectIdentifier
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(objectIdentifier)
     }
 }
